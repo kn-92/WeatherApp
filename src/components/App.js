147ba83b39fa97c1f11f9cde0559614b271d6ba9
Kamil_Niemiec <kamil_niemiec@o2.pl>
2021-06-04 15:47:33 +0200

    https api adress change

--- src/components/App.js
+++ src/components/App.js
@@ -28,7 +28,7 @@ class App extends Component {
 
   handleLocationSubmit = (e) => {
     e.preventDefault();
-    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIkey}&units=metric`
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIkey}&units=metric`
 
     fetch(API)
     .then(response => {
